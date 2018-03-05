1a9e5eccc0149ca4263affe92a5d5c2c9634a848
Emily Chatterjee <echatter@umich.edu>
2018-03-05 12:25:00 -0500

    changed rpn.py

--- rpn.py
+++ rpn.py
@@ -21,24 +21,24 @@ def calculate(arg):
     stack = list()
     for token in arg.split():
         try:
-            value = int(token)
-            stack.append(value)
+            token = int(token)
+            stack.append(token)
         except ValueError:
             function = operators[token]         
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        logger.debug(stack)
+        print(stack)
     
     if len(stack) != 1:
-        raise TypeError
-
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        print(calculate(input('rpn calc> ')))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
